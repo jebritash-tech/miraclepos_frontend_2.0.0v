2d0e42b2acf35e93e56a00226760233aaa6abdd4
jebritash-tech <jebritash@gmail.com>
2026-09-24 12:43:25 +0300

    Update offline-db.js

--- src/js/offline-db.js
+++ src/js/offline-db.js
@@ -1,8 +1,8 @@
 // offline-db.js
 import Dexie from 'dexie';
-const db = new Dexie('MiraclePOSDB');
+const db = new Dexie('MiraclePOSDB_Altelal');
 
-db.version(3).stores({
+db.version(6).stores({
     pending_sales: 'id, synced, shift_id, created_at',
     pending_refunds: 'id, synced, sale_id, created_at',
     cached_medicines: 'id, name, barcode',
